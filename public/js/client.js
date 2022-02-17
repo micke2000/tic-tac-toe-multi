f2f2c2d8a6640643d4c6251485ba1e3fa65de57b
micke2000 <michal.mickiewicz@pollub.edu.pl>
2022-02-17 21:57:22 +0100

    v2

---
+++ public/js/client.js
@@ -0,0 +1,106 @@
+const socket = io();
+const boxes = document.querySelectorAll("div.line div")
+const mainText = document.getElementById("main-text")
+let blocked = true
+let gameIsOn = false
+function makeCircle(){
+    const i = document.createElement('i')
+    i.classList.add('fa-regular','fa-circle')
+    return i    
+}
+function makeX(){
+    const i = document.createElement('i')
+    i.classList.add('fa-solid','fa-x')
+    return i    
+}
+let markGiven = "";
+socket.on("waiting-for-player",()=>{
+    mainText.textContent = "Waiting for second player..."
+})
+socket.on("your-turn",()=>{
+    socket.emit("unblock-me")
+    mainText.textContent = "Make your move!"
+    blocked = false
+})
+socket.on("game-ended",()=>{
+    socket.emit("player-left-after-game")
+    window.location.href = "../index.html";
+})
+socket.on("you-won",()=>{
+    mainText.textContent = "You won!"
+})
+socket.on("you-won-other-left",()=>{
+    mainText.textContent = "You won! Other player left!"
+})
+socket.on("other-player-won",()=>{
+    mainText.textContent = "You lost!"
+})
+socket.on("game-started",userId=>{
+    if(socket.id==userId){
+    mainText.textContent = "Make your first move!"
+    blocked = false;
+    socket.emit("first-one")
+    }
+    else{
+    mainText.textContent = "Wait for your move."
+    }
+    boxes.forEach(function(box){
+        box.addEventListener("mouseover",function(){
+            if(!blocked){
+            if(!box.classList.contains("clicked")){
+            box.appendChild(markGiven());
+            }
+        }
+        })
+        box.addEventListener("mouseout",function(){
+            if(!blocked){
+            if(!box.classList.contains("clicked")){
+            box.querySelector("i").remove()
+            }
+        }
+        })
+        box.addEventListener("click",function(){
+            if(!blocked){
+            if(!box.classList.contains("clicked")){
+            box.classList.add('clicked')
+            box.querySelector("i").remove()
+            socket.emit("clicked",box.id)
+            box.appendChild(markGiven());
+            }
+        }
+        })
+    })
+})
+socket.on("too-many-users",()=>{
+    alert("Przekroczono dozwolona liczbe graczy!")
+    window.location.href = "../index.html";
+})
+socket.on("draw",()=>{
+    mainText.textContent = "Draw!"
+})
+socket.on("blocked",()=>{
+    mainText.textContent = "Wait for your move."
+    blocked = true;
+})
+socket.on("mark-given",mark=>{
+    if(mark=="x"){
+        markGiven = makeX
+        alert(`You are X!`)
+    }
+    else{
+        markGiven = makeCircle
+        alert(`You are Sphere!`)
+    }
+    console.log(markGiven)
+})
+socket.on("box-marked",data=>{
+    console.log(`box ${data.id} marked`)
+    const box = document.getElementById(data.id)
+    box.classList.add('clicked')
+    if(data.mark=='x'){
+    box.appendChild(makeX())
+    }
+    else{
+        box.appendChild(makeCircle())
+    }
+})
--- server.js
+++ server.js
@@ -14,11 +14,170 @@ const io = socketIO(server)
 server.listen(PORT, () => console.log(`Server running on ${PORT}`))
 let connectedPlayers = 0
 let users = {}
-io.on('connection', socket => {
-    connectedPlayers ++; 
-    if(connectedPlayers>2){
-        socket.emit("too-many-users")
-        socket.disconnect();
+let userIds = []
+let current = "";
+let isSessionOn = false;
+let board = new Map([
+    [1, ""],
+    [2, ""],
+    [3, ""],
+    [4, ""],
+    [5, ""],
+    [6, ""],
+    [7, ""],
+    [8, ""],
+    [9, ""]
+]);
+const possibleWinnings = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 5, 9],
+    [3, 5, 7],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9]
+]
+
+function checkForWin(user) {
+    let win = false
+    const userBoxes = user.boxes.sort()
+    console.log(userBoxes)
+    possibleWinnings.forEach(function (winOption) {
+        if (userBoxes.join(',').includes(winOption.sort().join(","))) {
+            win = true;
+        }
+    })
+    return win
+}
+
+function allSet() {
+    console.log(board)
+    let allSet = true
+    const values = Array.from(board.values());
+    for (let i = 0; i < values.length; i++) {
+        if (values[i] == "") {
+            allSet = false
+        }
     }
-    console.log(socket.id)
+    return allSet;
+}
+
+function reset() {
+    board = board = new Map([
+        [1, ""],
+        [2, ""],
+        [3, ""],
+        [4, ""],
+        [5, ""],
+        [6, ""],
+        [7, ""],
+        [8, ""],
+        [9, ""]
+    ]);
+    console.log("reset")
+    isSessionOn = false
+    userIds = []
+    users = {}
+    resetSockets()
+}
+
+function resetSockets() {
+    const clients = io.sockets.sockets;
+    clients.forEach(function (c) {
+        c.disconnect();
+    });
+}
+setTimeout(() => {
+    const clients = io.sockets.sockets;
+    clients.forEach(function (c) {
+        if (!c.connected) {
+            delete users[c.id]
+            c.disconnect();
+        }
+    })
+}, 10000)
+io.on('connection', socket => {
+    if (connectedPlayers >= 2) {
+        socket.emit("too-many-users")
+        console.log('disc')
+        socket.disconnect();
+    } else {
+        connectedPlayers++;
+        console.log(socket.id)
+        if (connectedPlayers == 1) {
+            users[socket.id] = {
+                mark: "sph",
+                blocked: true,
+                boxes: []
+            }
+        } else {
+            users[socket.id] = {
+                mark: "x",
+                blocked: true,
+                boxes: []
+            }
+        }
+        socket.emit("mark-given", users[socket.id].mark)
+        userIds.push(socket.id)
+    }
+    if (connectedPlayers < 2) {
+        socket.emit("waiting-for-player")
+    } else {
+        var randId = userIds[Math.floor(Math.random() * userIds.length)];
+        users[randId].blocked = false
+        io.emit("game-started", randId)
+        isSessionOn = true
+    }
+    socket.on("clicked", boxId => {
+        if (board.get(Number(boxId)) == "" && !(users[socket.id].blocked)) {
+            board.set(Number(boxId), users[socket.id].mark)
+            socket.broadcast.emit('box-marked', {
+                id: boxId,
+                mark: users[socket.id].mark
+            })
+            socket.broadcast.emit('your-turn');
+            socket.emit('blocked')
+            users[socket.id].blocked = true
+            users[socket.id].boxes.push(boxId)
+            if (checkForWin(users[socket.id])) {
+                socket.emit("you-won")
+                socket.broadcast.emit('other-player-won')
+                setTimeout(() => {
+                    io.emit('game-ended')
+                }, 2000);
+            } else if (allSet()) {
+                socket.emit("draw");
+                socket.broadcast.emit('draw')
+                setTimeout(() => {
+                    io.emit('game-ended')
+                }, 2000);
+            }
+        }
+    })
+    socket.on("unblock-me", () => {
+        users[socket.id].blocked = false
+    })
+    socket.on("first-one", () => {
+        socket.broadcast.emit('blocked')
+    })
+    socket.on("player-left-after-game", () => {
+        resetSockets();
+    })
+    socket.on("disconnect", () => {
+        connectedPlayers--;
+        delete userIds[userIds.indexOf(socket.id)]
+        console.log("disconnect")
+        if (connectedPlayers == 0) {
+            reset();
+        }
+        if (connectedPlayers == 1 && isSessionOn) {
+            socket.broadcast.emit("you-won-other-left")
+            setTimeout(() => {
+                io.emit('game-ended')
+            }, 2000);
+            reset();
+        }
+    })
+    console.log(connectedPlayers)
 })
